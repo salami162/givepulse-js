3c643b68a53058dfc4989e7ecb1c4cfcf3f85146
Limin Shen <limin.shen@hotmail.com>
2012-06-17 22:58:00 -0500

    add user model

---
+++ public/scripts/models/user.js
@@ -0,0 +1,13 @@
+define([
+	'backbone'
+], function(Backbone){
+	
+	console.log("loading user model");
+	
+	return Backbone.Model.extend({
+    initialize : function () {
+    	console.log('user model initialized')
+    }
+	});
+	
+});
--- public/scripts/views/opportunity.js
+++ public/scripts/views/opportunity.js
@@ -14,12 +14,26 @@ define([
 
     initialize : function () {
       this.template = tmplHtml;
+			this.$el.on('click','button.givepulse-register-btn', this, this.registerOpportunity);
     },
 
-    render: function () {
+    render : function () {
       var html = this.template({opportunities : this.collection.toJSON()});
       console.log("render HTML = ", html);
       this.$el.html( html );
+    },
+    
+    registerOpportunity : function (event) {
+    	var curView = event.data;
+    	var curUser = curView.model.get('loginUser');
+    	if (!curUser.get('isAuthenticated')) {
+    		alert("please login first");
+    	}
+    	else {
+	    	var tr = $(this).parents('tr')[0];
+	    	var oppName = $(tr).attr('data-opp-name');
+    	}
+    	return false;
     }
   
   });
